306d5fbca73843a58d867b5ef75c12090b326562
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2020-10-10 17:51:50 +0200

    HealthBar update and Game Over Scene added.

--- ProtectorsOfTheNature/Assets/Scripts/TowerLayer.cs
+++ ProtectorsOfTheNature/Assets/Scripts/TowerLayer.cs
@@ -1,8 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class TowerLayer : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject _healthBar;
+    private Healthbar _healthBarScript;
+
+    private void Awake()
+    {
+        _healthBarScript = _healthBar.GetComponent<Healthbar>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("KamikazeEnemy"))
+        {
+            print("tree Triggered!");
+            _healthBarScript.TakeDamage(20);
+
+            if (_healthBarScript.health == 0)
+            {
+                SceneManager.LoadScene("GameOver");
+            }
+        }
+    }
 }
