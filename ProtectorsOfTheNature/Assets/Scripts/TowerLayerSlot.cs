2ea0e35bccc17f693774fcc49269802cf8e47cb7
mauu95 <mauu95@hotmail.it>
2020-10-10 17:03:22 +0200

    FIXED: Spawns 2 turret

--- ProtectorsOfTheNature/Assets/Scripts/TowerLayerSlot.cs
+++ ProtectorsOfTheNature/Assets/Scripts/TowerLayerSlot.cs
@@ -6,6 +6,8 @@ public class TowerLayerSlot : MonoBehaviour
 {
     private Weapon[] weapons;
 
+    private bool isEquipped = false;
+
     private void Start()
     {
         weapons = FindObjectOfType<WeaponsManager>().weapons;
@@ -15,10 +17,13 @@ public class TowerLayerSlot : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            if (isEquipped)
+                return;
+
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
             if (hit.collider != null && hit.collider.gameObject.GetComponent<TowerLayerSlot>() != null)
             {
-                Instantiate(weapons[0].gameObject, transform.position, Quaternion.identity);
+                Instantiate(weapons[0].gameObject, transform);
                 GetComponent<SpriteRenderer>().enabled = false;
             }
         }
