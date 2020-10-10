6b5eb98804157d1231dd00fab94f29fac5cd208c
mauu95 <mauu95@hotmail.it>
2020-10-10 18:09:18 +0200

    Added green & yellow sparamele

--- ProtectorsOfTheNature/Assets/Scripts/TowerLayer.cs
+++ ProtectorsOfTheNature/Assets/Scripts/TowerLayer.cs
@@ -10,19 +10,16 @@ public class TowerLayer : MonoBehaviour
     [SerializeField] private GameObject _healthBar;
     private Healthbar _healthBarScript;
 
-    private void Awake()
-    {
-        _healthBarScript = _healthBar.GetComponent<Healthbar>();
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
+        _healthBarScript = FindObjectOfType<Healthbar>();
         if (other.CompareTag("KamikazeEnemy"))
         {
             _healthBarScript.TakeDamage(20);
             if (_healthBarScript.health == 0)
             {
-                SceneManager.LoadScene("GameOver");
+                print("You lost bro");
+                //SceneManager.LoadScene("GameOver");
             }
         }
     }
