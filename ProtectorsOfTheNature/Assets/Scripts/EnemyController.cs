7a24c92777158dfc4d863f0c866153b022321bf1
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2020-10-10 15:32:59 +0200

    Kamikaze and Level Manager Added
    Created Kamikaze Enemy
    Created Level Manager

---
+++ ProtectorsOfTheNature/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] [Range(2.0f, 10.0f)] private float _speed;
+
+    private Vector3 initialPosition;
+
+    // Caching! Use this variable instead of the traditional one.
+    private Transform _transform;
+
+
+    private void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    private void Awake()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (initialPosition.x < 0)
+        {
+            _transform.position += _speed * Time.fixedDeltaTime * transform.right;
+        }
+        else _transform.position += _speed * Time.fixedDeltaTime * -transform.right;
+    }
+}
---
+++ ProtectorsOfTheNature/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private Transform[] _spawnPoints;
+    
+    [Header("Enemy Prefabs")]
+    [SerializeField] private GameObject _enemyPrefab;
+    
+    [Header("Enemy Number")]
+    [SerializeField] [Range(1.0f, 20.0f)] private int _numberOfKamikaze;
+
+    private void Start()
+    {
+        StartCoroutine(GenerateKamikazeEnemy());
+    }
+
+    private IEnumerator GenerateKamikazeEnemy()
+    {
+        for (int i = 0; i < _numberOfKamikaze; i++)
+        {
+            Transform spawnPoint = _spawnPoints[Random.Range(0, _spawnPoints.Length)];
+            Vector3 position = new Vector3(spawnPoint.position.x, spawnPoint.position.y, spawnPoint.position.z);
+
+            GameObject enemyGameObject = Instantiate(_enemyPrefab, position, Quaternion.identity);
+
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
