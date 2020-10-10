d39adeb9805f517cd1af392d1b8787bd7950e7e3
mauu95 <mauu95@hotmail.it>
2020-10-10 13:17:02 +0200

    Added basic weapon (sparamele)

---
+++ ProtectorsOfTheNature/Assets/Bullet_Apple.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Apple : MonoBehaviour
+{
+    public float speed = 1f;
+
+    private Vector3 target;
+
+    private void Update()
+    {
+        if (transform.position == target)
+            Destroy(gameObject);
+
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+    }
+
+    public void SetTarget(Vector3 pos)
+    {
+        target = pos;
+    }
+}
