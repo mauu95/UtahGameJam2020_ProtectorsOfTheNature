e78bd16d3628a1cd4207e6b46f67dce98a033d70
mauu95 <mauu95@hotmail.it>
2020-10-10 23:31:04 +0200

    Added Shield patata

---
+++ ProtectorsOfTheNature/Assets/W_ShieldShooter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class W_ShieldShooter : Weapon
+{
+    public GameObject bulletPrefab;
+    public float force = 1f;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null && hit.collider.gameObject == gameObject)
+            {
+                GameObject gm = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+                Rigidbody2D rb = gm.GetComponent<Rigidbody2D>();
+                rb.AddForce(Vector2.right * force);
+            }
+        }
+    }
+}
