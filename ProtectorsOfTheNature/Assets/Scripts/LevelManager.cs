7a4856ac2415b108254952b7f3f8556e1d58354f
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2020-10-10 19:31:24 +0200

    Refactoring

--- ProtectorsOfTheNature/Assets/Scripts/LevelManager.cs
+++ ProtectorsOfTheNature/Assets/Scripts/LevelManager.cs
@@ -16,10 +16,14 @@ public class LevelManager : Singleton<LevelManager>
 
 
     [Header("Enemy Prefabs")] [SerializeField]
-    private GameObject _enemyPrefab;
+    private GameObject _kamikazePrefab;
+
+    [SerializeField] private GameObject _planePrefab;
 
     [Header("Enemy Number")] [SerializeField] [Range(0.0f, 20.0f)]
     private int _numberOfKamikaze;
+
+    [SerializeField] [Range(0.0f, 20.0f)] private int _numberOfPlanes;
 
     [Header("Initial Game State")] [SerializeField] [Range(100, 250)]
     private int _initialMoney;
@@ -29,16 +33,28 @@ public class LevelManager : Singleton<LevelManager>
 
     private void Start()
     {
-        if (_numberOfKamikaze > 0)
-        {
-            StartCoroutine(GenerateKamikazeEnemy());
-        }
+        if (_numberOfKamikaze > 0) StartCoroutine(GenerateKamikaze());
+        if (_numberOfPlanes > 0) StartCoroutine(GeneratePlanes());
 
 
         StartCoroutine(AutoUpdateMoney());
 
         moneyTextField.text = _moneyBaseText + " " + _initialMoney;
         _actualMoney = _initialMoney;
+    }
+
+    private IEnumerator GeneratePlanes()
+    {
+        for (int i = 0; i < _numberOfPlanes; i++)
+        {
+            Transform spawnPoint = _spawnPoints[Random.Range(2, 3)];
+            Vector3 position = new Vector3(spawnPoint.position.x, spawnPoint.position.y, spawnPoint.position.z);
+
+            GameObject enemyGameObject = Instantiate(_planePrefab, position, Quaternion.identity);
+        }
+        
+        
+        yield return new WaitForSeconds(Random.Range(0.5f, 2.0f));
     }
 
     private IEnumerator AutoUpdateMoney()
@@ -50,16 +66,16 @@ public class LevelManager : Singleton<LevelManager>
         }
     }
 
-    private IEnumerator GenerateKamikazeEnemy()
+    private IEnumerator GenerateKamikaze()
     {
         for (int i = 0; i < _numberOfKamikaze; i++)
         {
-            Transform spawnPoint = _spawnPoints[Random.Range(0, _spawnPoints.Length)];
+            Transform spawnPoint = _spawnPoints[Random.Range(0, 1)];
             Vector3 position = new Vector3(spawnPoint.position.x, spawnPoint.position.y, spawnPoint.position.z);
 
-            GameObject enemyGameObject = Instantiate(_enemyPrefab, position, Quaternion.identity);
+            GameObject enemyGameObject = Instantiate(_kamikazePrefab, position, Quaternion.identity);
 
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(Random.Range(0.5f, 2.0f));
         }
     }
 
