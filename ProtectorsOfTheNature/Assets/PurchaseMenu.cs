14e4b4740daf44ce157d841c206cddf887441d16
mauu95 <mauu95@hotmail.it>
2020-10-11 10:45:00 +0200

    Added purhase menu UI

---
+++ ProtectorsOfTheNature/Assets/PurchaseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PurchaseMenu : MonoBehaviour
+{
+    private TowerLayerSlot currentSlot;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void BuyAppleShooter()
+    {
+        currentSlot.EquipAppleShooter();
+    }
+
+    public void BuyShieldShooter()
+    {
+        currentSlot.EquipShieldShooter();
+    }
+
+    public void SetSlot(TowerLayerSlot slot)
+    {
+        currentSlot = slot;
+    }
+}
