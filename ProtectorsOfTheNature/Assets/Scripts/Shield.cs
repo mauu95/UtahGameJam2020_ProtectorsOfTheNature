7f81e4a2ba2a44db79d8dd2a7a868a3cf0789b9f
mauu95 <mauu95@hotmail.it>
2020-10-11 21:48:29 +0200

    Shield Done

--- ProtectorsOfTheNature/Assets/Scripts/Shield.cs
+++ ProtectorsOfTheNature/Assets/Scripts/Shield.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Shield : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int health = 5;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.GetComponent<EnemyController>())
+        {
+            Destroy(collision.gameObject);
+            health--;
+        }
+
+        if (health <= 0)
+            Destroy(gameObject);
     }
 }
