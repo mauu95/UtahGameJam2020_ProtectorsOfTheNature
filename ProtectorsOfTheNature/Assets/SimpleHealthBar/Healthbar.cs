4a045c6ac5b4c87495c84e87d374ab5f03ececc9
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2020-10-11 16:51:11 +0200

    Added slider to modify damage dealt by enemies

--- ProtectorsOfTheNature/Assets/SimpleHealthBar/Healthbar.cs
+++ ProtectorsOfTheNature/Assets/SimpleHealthBar/Healthbar.cs
@@ -115,7 +115,7 @@ public class Healthbar : MonoBehaviour {
         UpdateHealth();
     }
 
-    public void TakeDamage(int amount)
+    public void TakeDamage(float amount)
     {
         // Remove 'amount' hitpoints, then update the characters health:
         health -= float.Parse(amount.ToString());
