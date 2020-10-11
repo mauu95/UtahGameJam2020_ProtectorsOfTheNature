4d7a26babe3c24a1dfe4952a622be341e5585d3b
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2020-10-11 22:34:04 +0200

    Added WinScene and changed UI in general

---
+++ ProtectorsOfTheNature/Assets/Scripts/WinSceneScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinSceneScript : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ToGameMenu()
+    {
+        SceneManager.LoadScene("IntroMenu");
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
